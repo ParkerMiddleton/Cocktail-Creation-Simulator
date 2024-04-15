1d0e5a5a1c66d932cf7440e9dccebf4de638f1b7
ForkBombRaja <adityamukerjee621311@gmail.com>
2024-04-15 17:48:49 -0600

    addes some methods to model

--- BarBlunder/recipe.cpp
+++ BarBlunder/recipe.cpp
@@ -23,3 +23,23 @@ recipe::recipe(QTextStream* recipe) {
 recipe::~recipe(){
 
 }
+void recipe::addIngredient(QString ingredient, int amount){
+    ingredients.push_back(qMakePair(ingredient, amount));//add a pair to ingredients
+}
+bool recipe::checkRecipe(recipe drinkRecipe){
+    /*
+     * make sure both lists of ingredients are the same size
+     */
+    if (ingredients.size() != drinkRecipe.ingredients.size()) {
+        return false;
+    }
+    /*
+     * Check each Qpair in the drink is identical to the recipe
+     */
+    for (int i = 0; i < ingredients.size(); ++i) {
+        if ((ingredients[i].first != drinkRecipe.ingredients[i].first)||(ingredients[i].second != drinkRecipe.ingredients[i].second)){
+            return false;
+        }
+    }
+    return true;//if we got this far in the method return true
+}
--- BarBlunder/recipe.h
+++ BarBlunder/recipe.h
@@ -9,6 +9,19 @@ public:
     ~recipe();
     QString drinkName;
     QList<QPair<QString, int>> ingredients;
+    /**
+     * @brief This adds a specifird amount of the ingridient to the drink object
+     * @param ingredient-the ingredient represented as a QString
+     * @param amount-the amount of the ingredient
+     */
+    void addIngredient(QString ingredient, int amount);
+    /**
+     * @brief This method checks the users drink against the recipe
+     * @param drinkRecipe - the actual correct recipe ffor the drink
+     * @return true if the drink matches the recipe falsse if it does not
+     */
+    bool checkRecipe(recipe drinkRecipe);
+
 };
 
 #endif // RECIPE_H
