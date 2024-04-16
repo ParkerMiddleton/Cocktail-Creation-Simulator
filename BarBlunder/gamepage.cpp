abaede1c7ed91f9bc05cafe6b31a82bb17c2df1a
JennyCraigslist <122509962+JennyCraigslist@users.noreply.github.com>
2024-04-15 22:37:44 -0600

    Here are the art assets :)

--- BarBlunder/gamepage.cpp
+++ BarBlunder/gamepage.cpp
@@ -10,7 +10,7 @@ GamePage::GamePage(BarModel *bar, QWidget *parent)
 	////////////// ICONS FOR BUTTONS /////////////////////
 
 	// background image
-	QPixmap bkgImage(":/images/bar.png");
+    QPixmap bkgImage(":/images/ButtonIcons/Background/bar.png");
 	int backgroundHeight = 400;
 	int backgroundWidth = 1156;
 	bkgImage = bkgImage.scaled(backgroundWidth, backgroundHeight, Qt::KeepAspectRatioByExpanding);
@@ -18,35 +18,35 @@ GamePage::GamePage(BarModel *bar, QWidget *parent)
 
 
 	//Vodka
-	QIcon vodkaIcon(":/images/titos.png");
+    QIcon vodkaIcon(":/images/ButtonIcons/Liquor/titos.png");
 	ui->VodkaButton->setIcon(vodkaIcon);
 	ui->VodkaButton->setSizePolicy(QSizePolicy::Preferred, QSizePolicy::Preferred);
 	QSize vodkaButtonSize(ui->VodkaButton->size());
 	ui->VodkaButton->setIconSize(vodkaButtonSize * 2.85);
 
 	//Tequila
-	QIcon tequilaIcon(":/images/tequila.png");
+    QIcon tequilaIcon(":/images/ButtonIcons/Liquor/tequila.png");
 	ui->TequilaButton->setIcon(tequilaIcon);
 	ui->TequilaButton->setSizePolicy(QSizePolicy::Preferred, QSizePolicy::Preferred);
 	QSize tequilaButtonSize(ui->TequilaButton->size());
 	ui->TequilaButton->setIconSize(tequilaButtonSize * 2.8);
 
 	//Gin
-	QIcon ginIcon(":/images/gin.png");
+    QIcon ginIcon(":/images/ButtonIcons/Liquor/gin.png");
 	ui->GinButton->setIcon(ginIcon);
 	ui->GinButton->setSizePolicy(QSizePolicy::Preferred, QSizePolicy::Preferred);
 	QSize ginButtonSize(ui->GinButton->size());
 	ui->GinButton->setIconSize(ginButtonSize * .85);
 
 	//Burbon
-	QIcon burbonIcon(":/images/burbon.png");
+    QIcon burbonIcon(":/images/ButtonIcons/Liquor/burbon.png");
 	ui->BurbonButton->setIcon(burbonIcon);
 	ui->BurbonButton->setSizePolicy(QSizePolicy::Preferred, QSizePolicy::Preferred);
 	QSize burbonButtonSize(ui->BurbonButton->size());
 	ui->BurbonButton->setIconSize(burbonButtonSize * 2.3);
 
 	//Rum
-	QIcon rumIcon(":/images/rum.png");
+    QIcon rumIcon(":/images/ButtonIcons/Liquor/rum.png");
 	ui->RumButton->setIcon(rumIcon);
 	ui->RumButton->setSizePolicy(QSizePolicy::Preferred, QSizePolicy::Preferred);
 	QSize rumButtonSize(ui->RumButton->size());
@@ -60,7 +60,7 @@ GamePage::GamePage(BarModel *bar, QWidget *parent)
     ui->LimeWedgeButton->setIconSize(limeButtonSize*1.3);
 
 	// Orange Peele container
-	QIcon orangeIcon(":/images/orangePeeles.png");
+    QIcon orangeIcon(":/images/ButtonIcons/Garnish/orangePeeles.png");
 	ui->OrangeButton->setIcon(orangeIcon);
 	ui->OrangeButton->setSizePolicy(QSizePolicy::Preferred, QSizePolicy::Preferred);
 	QSize orangeButtonSize(ui->OrangeButton->size());
@@ -74,21 +74,21 @@ GamePage::GamePage(BarModel *bar, QWidget *parent)
 	ui->OliveButton->setIconSize(oliveButtonSize*1.7);
 
 	//bitters bottle
-	QIcon bittersIcon(":/images/bitters.png");
+    QIcon bittersIcon(":/images/ButtonIcons/Garnish/olives.png");
 	ui->BittersButton->setIcon(bittersIcon);
 	ui->BittersButton->setSizePolicy(QSizePolicy::Preferred, QSizePolicy::Preferred);
 	QSize BittersButtonSize(ui->BittersButton->size());
 	ui->BittersButton->setIconSize(BittersButtonSize*1.7);
 
 	//shakertin
-	QIcon shakertinIcon(":/images/shakertin.png");
+    QIcon shakertinIcon(":/images/ButtonIcons/Tools/shakertin.png");
 	ui->ShakertinButton->setIcon(shakertinIcon);
 	ui->ShakertinButton->setSizePolicy(QSizePolicy::Preferred, QSizePolicy::Preferred);
 	QSize shakertinButtonSize(ui->ShakertinButton->size());
 	ui->ShakertinButton->setIconSize(shakertinButtonSize*.95);
 
 	// ice cubes
-	QIcon iceIcon(":/images/ice.png");
+    QIcon iceIcon(":/images/ButtonIcons/Ingredients/ice.png");
 	ui->IceButton->setIcon(iceIcon);
 	ui->IceButton->setSizePolicy(QSizePolicy::Preferred, QSizePolicy::Preferred);
 	QSize iceButtonSize(ui->IceButton->size());
